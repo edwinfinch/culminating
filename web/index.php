0efdd9dd0ddb521ef35fdc46d8ce87340408611a
zzq1015 <zzq1015@users.noreply.github.com>
2015-06-16 10:53:51 -0400

    init
    The first version

---
+++ web/index.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html>
+        <head>
+                <title>Our Project</title>
+                <meta charset="utf-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1">
+                <script type="text/javascript" src="proj.js"></script>
+        </head>
+        <body topmargin="30" leftmargin="30" rightmargin="30" bottommargin="30" align="center">
+
+                <h1><font face="Times New Roman">Our Culminating Project.</font></h1>
+                <p><font face="Arial">Here is our culminating project. <a href="/">Go back to Home page</a>.</p>
+                <p>Click one botton to move the robot to a desired direction.</p>
+                <button type="button" onclick="stop()">Stop</button>
+                <button type="button" onclick="forward()">Forward</button>
+                <button type="button" onclick="backward()">Backward</button>
+                <button type="button" onclick="right()">Right</button>
+                <button type="button" onclick="left()">Left</button>
+                <p id="request_state">Click any button to move.</p>
+                </font>
+        </body>
+</html>
